ac805293da700a58d0daee3add0f761e72383fff
ltr2020 <63619930+ltr2020@users.noreply.github.com>
2022-01-05 12:42:07 +0800

    Time: 40 ms (93.99%), Space: 14.4 MB (24.24%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
         table = {}
-        # x + y = target
-        # target - num in table
         for i, val in enumerate(nums):
             if target - val in table:
                 return table[target - val], i
