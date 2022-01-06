a7d426c8d76d9efaccc3faa9dbb3cd03c3342d55
ltr2020 <63619930+ltr2020@users.noreply.github.com>
2022-01-07 01:52:43 +0800

    Time: 47 ms (19.85%), Space: 14.2 MB (82.14%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,10 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        
+        
         strs.sort() # sort the string according to a-z
-        end = min((len(strs[0])), len(strs[len(strs)-1]))
+        end = min((len(strs[0])), len(strs[-1]))   # reduce time
         
         i = 0
-        while i<end and strs[0][i] == strs[len(strs)-1][i]:
+        while i < end and strs[0][i] == strs[-1][i]: 
+        # compare the first and last string since if there's a matching prefix between first and last string,               strings in b/w will contain that prefix too
             i += 1
         
         pre = strs[0][0: i]
